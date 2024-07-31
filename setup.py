7b1c7fb75eeab9b5a32a1335d32a2b7d4fd7c3da
Ronan Abhamon <ronan.abhamon@vates.tech>
2024-07-31 15:53:00 +0200

    Add a setup.py script to compile python scripts
    Signed-off-by: Ronan Abhamon <ronan.abhamon@vates.tech>

---
+++ setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+#
+# Copyright (C) 2022  Vates SAS
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+from setuptools import setup
+
+setup(
+    name="http-nbd-transfer",
+    version="1.4.0",
+    description="Set of tools to transfer NBD requests to an HTTP server",
+    author="Ronan Abhamon <ronan.abhamon@vates.tech>",
+    author_email="ronan.abhamon@vates.tech",
+    url="https://vates.tech",
+    license="GPLv3",
+    py_modules=["http-disk-server", "nbd-http-server"],
+    scripts=["scripts/http-disk-server", "scripts/nbd-http-server"]
+)
